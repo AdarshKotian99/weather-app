187bd30c2aab776dd9bfbcbc3c32ede9633961c9
Adarsh Kotian <blade@example.com>
2024-08-18 21:45:54 +0530

    changes in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { appConfig,apiConfig } from '../config';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Input() currentUnitSystem!:string;
+  @Output() changeUnit: EventEmitter<string> = new EventEmitter();
+
+  isUnitSwitcherChecked = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isUnitSwitcherChecked = this.currentUnitSystem === appConfig.defaultUnit;
+  }
+
+  onChangeUnitSwitcher(){
+    const unitSystems = Object.keys(apiConfig.measurementUnits);
+    const unitIndex = this.isUnitSwitcherChecked ? 1 :0;
+    this.changeUnit.emit(unitSystems[unitIndex]);
+
   }
 
 }
