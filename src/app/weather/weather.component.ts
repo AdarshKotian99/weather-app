94a959fb4eefaa856d0839c06c8f9f7317419342
Adarsh Kotian <blade@example.com>
2024-08-20 22:25:09 +0530

    add new changes to services

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Weather } from './weather';
+import { WeatherService } from '../services/weather.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  weather !: Weather;
+  private _weatherSubscription!: Subscription;
+  constructor(private route : ActivatedRoute , private weatherService: WeatherService) {  }
 
   ngOnInit(): void {
+    this.route.data.subscribe(
+      (data) => {
+        if(data['weather'] == null){
+          //this.router.navigate(['/error']); //review logic
+        }else{
+          this.weather = data['weather'];
+        } 
+      }
+    );
+
+    this._weatherSubscription = this.weatherService.getWeather().subscribe(weather => {
+      this.weather = weather;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._weatherSubscription.unsubscribe();
   }
 
 }
